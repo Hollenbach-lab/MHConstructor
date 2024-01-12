6f40681b2be993810ab169ee7d0728ac4b54c022
rsuseno2907 <benediktusrayosuseno@gmail.com>
2024-01-12 00:44:53 +0000

    added t1k processing up to HLAgenotypes.csv creation

---
+++ genotypes/genotypeDRB.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+## Wrapper script to obtain the DRB genotypes needed for MHConstructor to run
+## Usage: sh genotypeDRB.sh
+## The fastq files location can be specified in ../control.txt
+## The sample ID to run can be modified in ../testID.txt
+
+source ../control.txt
+repoDir=${binDir}/MHConstructor
+progT1K=${repoDir}/tools/T1K/run-t1k
+ids="$(cat ${repoDir}/testID.txt)"
+
+for i in $ids; do
+    echo $i
+    ${progT1K} -1 ${targetCapture}/${i}_${R1ext} -2 ${targetCapture}/${i}_${R2ext} --preset hla -f ${repoDir}/tools/T1K/DRB1_IMGT_010324.fasta -o ${i} -t 8 --skipPostAnalysis --od ./alleles
+    cd ./alleles
+    find . ! -name '*_allele.tsv' -type f -exec rm -f {} +
+    cd ../
+done
+
+python prepDRB.py ${repoDir}/genotypes/alleles
---
+++ genotypes/prepDRB.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+import sys
+
+# A supporting script for genotypeDRB.sh to format the HLA-DRB1 genotypes to a nice tabular format
+
+def main():
+    # alleles = "/home/rsuseno/MHConstructor/genotypes/alleles"
+    alleles = sys.argv[1]
+    directory = os.fsencode(alleles)
+    drb11 = []
+    drb12 = []
+    sid = []
+
+    for file in os.listdir(directory):
+        file = file.decode('utf-8')
+        sid.append(file.split('_')[0])
+        toread = f'{alleles}/{file}'
+        allele = pd.read_csv(toread, sep = '\t', header = None)
+        tmp1=(allele.iloc[0,0].split(' ')[0].split('*')[1])
+        tmp2=(allele.iloc[1,0].split(' ')[0].split('*')[1])
+
+        # Limit the result to only include 2 resolutions: cutting it off if resolution>2
+        test1 = len(tmp1.split(':'))
+        test2 = len(tmp2.split(':'))
+        if test1 > 2:
+            spl1 = tmp1.split(':')
+            tmp1 = spl1[0]+':'+spl1[1]
+        if test2 > 2:
+            spl2 = tmp2.split(':')
+            tmp2 = spl2[0]+':'+spl2[1]
+        
+        drb11.append(tmp1)
+        drb12.append(tmp2)
+
+    final_df = pd.DataFrame()
+    final_df['SampleID'] = sid
+    final_df['HLA-DRB1.1'] = drb11
+    final_df['HLA-DRB1.2'] = drb12
+    final_df.to_csv('HLAgenotypes.csv', index=False)
+
+main()
