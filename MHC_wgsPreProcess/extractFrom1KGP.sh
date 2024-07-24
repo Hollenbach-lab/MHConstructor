b8df650a3cfac10bceef8960c8e2f265d6bcfe93
rsuseno2907 <benediktusrayosuseno@gmail.com>
2024-07-24 17:53:49 +0000

    fix onekgp retrieval

--- MHC_wgsPreProcess/extractFrom1KGP.sh
+++ MHC_wgsPreProcess/extractFrom1KGP.sh
@@ -14,12 +14,16 @@ progSamtools=${4}
 altChroms="$(cat GRCH38chroms_altMHC.txt)"
 #ftp=$(python getFTP.py ${1} ${2} 2>&1)
 
+echo 'Downloading reference fasta'
+wget -nc https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa
+echo 'Finished downloading reference fasta'
+
 # Extract the ftp link associated with a sample
 
 ftp=$(less ${2} | grep ${1} )  
 echo $ftp
-mkdir ${5}/extractedReads
-${progSamtools} view -f 1 -h -T ../refMHC/GRCh38_full_analysis_set_plus_decoy_hla.fa -b ${ftp} ${3}| ${progSamtools} sort - -n -o ${5}/extractedReads/${1}_mhc_paired.sorted.bam
+mkdir -p ${5}/extractedReads
+${progSamtools} view -f 1 -h -T ./GRCh38_full_analysis_set_plus_decoy_hla.fa -b ${ftp} ${3}| ${progSamtools} sort - -n -o ${5}/extractedReads/${1}_mhc_paired.sorted.bam
 
 ${progSamtools} fastq -n ${5}/extractedReads/${1}_mhc_paired.sorted.bam -1 ${5}/extractedReads/${1}_mhc_paired_R1.fastq -2 ${5}/extractedReads/${1}_mhc_paired_R2.fastq -0 /dev/null -s /dev/null
 
@@ -28,12 +32,12 @@ for i in ${altChroms};
 do
 	altLoc="$(echo $i | cut -d '>' -f 2,2)"
 	echo ${altLoc}
-	${progSamtools} view -f 1 -h -T ../refMHC/GRCh38_full_analysis_set_plus_decoy_hla.fa -b ${ftp} ${altLoc} | ${progSamtools} sort - -n -o ${5}/extractedReads/${1}_mhc_${altLoc}_paired.sorted.bam
+	${progSamtools} view -f 1 -h -T ./GRCh38_full_analysis_set_plus_decoy_hla.fa -b ${ftp} ${altLoc} | ${progSamtools} sort - -n -o ${5}/extractedReads/${1}_mhc_${altLoc}_paired.sorted.bam
 	${progSamtools} fastq -n ${5}/extractedReads/${1}_mhc_${altLoc}_paired.sorted.bam -1 ${5}/extractedReads/${1}_mhc_${altLoc}_paired_R1.fastq -2 ${5}/extractedReads/${1}_mhc_${altLoc}_paired_R2.fastq -0 /dev/null -s /dev/null
 done;
 
 ## Get unmapped reads.bam
-${progSamtools} view -h -T ../refMHC/GRCh38_full_analysis_set_plus_decoy_hla.fa -b ${ftp} '*' | ${progSamtools} sort - -n -o ${5}/extractedReads/${1}_unmap.sorted.bam
+${progSamtools} view -h -T ./GRCh38_full_analysis_set_plus_decoy_hla.fa -b ${ftp} '*' | ${progSamtools} sort - -n -o ${5}/extractedReads/${1}_unmap.sorted.bam
 
 mv *.crai ${5}
 
